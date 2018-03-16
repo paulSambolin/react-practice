d9e3a0d169d2fa8d117a552667d4b42fe81f95dd
paulSambolin <paulsambolin@gmail.com>
2018-03-16 13:38:53 -0400

    Local Development Setup using facebook/create-react-app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Header/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p className="App-sub-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
